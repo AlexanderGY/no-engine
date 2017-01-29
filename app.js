ecf86c4c642c72af9f154bc1e647409dfd5d00ee
AlexanderGY <obsmyprof@gmail.com>
2017-01-29 16:43:24 +0300

    #1 init

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express'),
+   expressSession = require('express-session'),
+   path = require('path'),
+   favicon = require('serve-favicon'),
+   logger = require('morgan'),
+   cookieParser = require('cookie-parser'),
+   bodyParser = require('body-parser'),
+   mongoose = require('mongoose'),
+   routes = require('./routes/index'),
+   app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(expressSession({
+  secret: 'Omg'
+}));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.locals.current = req.session.current;
+  next();
+});
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+  res.locals.current = req.session.current;
+  next();
+});
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,106 @@
+'use strict';
+let express = require('express'),
+    router = express.Router(),
+    isPostRegExp = /\w+/g,
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  login: String,
+  password: String
+})
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  text: String,
+  link: String
+})
+
+let Post = mongoose.model('post', postSchema);
+let User = mongoose.model('user', userSchema);
+
+mongoose.connect('mongodb://localhost/test');
+
+router
+  .get('*', function(req, res, next) {
+    console.log(req.session);
+    if (!req.session.current) {
+      req.session.current = new Object();
+    }
+    let currentRequestUrl = req.originalUrl;
+    switch (currentRequestUrl) {
+      case '/':
+        Post.find({}, function(err, posts) {
+          res.render('index', { title: 'Main', data: posts });
+        })
+        break;
+      case '/login/':
+        res.render('login', { title: 'Login' });
+        break;
+      case '/publish/':
+        if (req.session.current.login) {
+          res.render('publish', { title: 'Publish' });
+        } else {
+          res.redirect('/login/');
+        }
+        break;
+      case '/exit/':
+        if (req.session.current.login) {
+          delete req.session.current.login;
+          res.redirect('/');
+        }
+        break;
+      default:
+        if (req.path.match(isPostRegExp)) {
+          Post.find({link: req.path.replace(/\//g,'')}, function(err, article) {
+            if (article.length) {
+              res.render('post', {title: article[0].title, text: article[0].text});
+            } else {
+              res.status(404).render('error', { title: 'Page not found', message: 'Page not found', error: {status: '404'} });
+            }
+          })
+        }
+    }
+  })
+  .post('*', function(req, res, next) {
+    if (!req.session.current) {
+      req.session.current = new Object();
+    }
+    let currentRequestUrl = req.originalUrl;
+    switch (currentRequestUrl) {
+      case '/registration/':
+        let user = User(req.body);
+        user.save(function(err) {
+          res.redirect('/');
+        })
+        break;
+      case '/publish/':
+        let article = Post({
+          title: req.body.title,
+          text: req.body.text,
+          link: req.body.title.toLowerCase().replace(/[^\w]/gi, '-')
+        });
+        article.save(function(err) {
+          res.redirect('/');
+        })
+        break;
+      case '/login/':
+        User.find({login: req.body.login}, function(err, user) {
+          if (user.length) {
+            if (user[0].password == req.body.password) {
+              req.session.current = {
+                login: user[0].login
+              }
+              res.redirect('/');
+            }
+          } else {
+            res.render('login', { title: 'Login', error: 'Login or password doesn`t exist' });
+          }
+        })
+        break;
+      default:
+        res.status(403).send('Error');
+    }
+  })
+
+module.exports = router;
